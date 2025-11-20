a0854f5bdc8a500735e69d34e5f623b4d2267b7c
Hlat Economia <43037433+Denon2010@users.noreply.github.com>
2025-11-20 23:08:13 +0100

    Add files via upload

---
+++ calculadora.java
@@ -0,0 +1,28 @@
+package com.daw.emulador;
+
+/**
+ * rama2
+ * @author Rubén
+ */
+public class calculadora {
+    public static void main(String[]args) {
+        // VARIABLES (LOCAL)
+        int a = 6;
+        int b = 3;
+        // Suma
+        int sum =  a + b; 
+        System.out.println("Addition: " + a + " + " + b + " = " + sum);
+        // Resta
+        int substraction  = a - b;
+        System.out.println("Substraction: " + a + " - " + b + " = " + substraction);
+        // Multiplicacion
+        int multiplication = a * b;
+        System.out.println("Multiplication: " + a + " * " + b + " = " + multiplication);
+        // Division
+        int division = a / b;
+        System.out.println("Division: " + a + " / " + b + " = " + division);
+                
+        
+
+    }
+}
